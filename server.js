96e8cc1090a335b0d70ea533060b8d237e7fc3eb
Robert Jones <robert.g.jones@gmail.com>
2018-04-03 16:59:38 +0100

    Use Express to create server and offer endpoint
    Post endpoint reflects request back and send request as a payload to
    Pusher API via instance in controller.

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-import Pusher from "pusher";
-import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
-
+require("dotenv").config();
+const Pusher = require("pusher");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 
 // apply cors middleware to enable CORS
@@ -10,6 +10,24 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// create new Pusher instance
 const pusher = new Pusher({
-  appId: "APP_ID"
+  appId: process.env.APP_ID,
+  key: process.env.APP_KEY,
+  secret: process.env.APP_SECRET,
+  cluster: process.env.APP_CLUSTER,
+  encrypted: true
 });
+
+app.set("PORT", process.env.PORT || 5001);
+
+app.post("/message", (req, res) => {
+  console.log("hit post endpoint");
+  const payload = req.body;
+  pusher.trigger("chat", "message", payload);
+  res.send(payload);
+});
+
+app.listen(app.get("PORT"), () =>
+  console.log("Listening at " + app.get("PORT"))
+);
